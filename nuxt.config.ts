2d932ff2059f3303438a2e10f8927243fb98ae8e
gj-wes <negativegary@gmail.com>
2023-09-21 11:14:41 +0100

    Added fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
+  googleFonts: {
+    families: {
+      Antonio: [500],
+      Spartan: [400, 700]
+    }
+  }
 })
